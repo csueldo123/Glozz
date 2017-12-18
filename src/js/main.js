b16616ed015a9c80733553fd13681d58f55dc212
Carlos Sueldo <csueldo@outlook.com>
2017-12-18 11:04:27 -0700

    added video section, autoplay javascript

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+    //found on stack overflow
+    //Auto Play Modal Video
+    $(".video").click(function () {
+        var theModal = $(this).data("target"),
+        videoSRC = $(this).attr("data-video"),
+        videoSRCauto = videoSRC +
+        "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' ifram').attr('src', videoSRC);
+        });
+    });
+});
